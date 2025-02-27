cf2eda321a82d55ea82e3ba10a8a480cdccfe650
yaene <yannis.bolliger@protonmail.com>
2025-02-27 22:11:30 +0100

    feat: allow specifying of cuttlefish path

--- src/entry.sh
+++ src/entry.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+: "${CF_HOME:=/cuttlefish}"
+
 info () { printf "%b%s%b" "\E[1;34m❯ \E[1;36m" "${1:-}" "\E[0m\n"; }
 error () { printf "%b%s%b" "\E[1;31m❯ " "ERROR: ${1:-}" "\E[0m\n" >&2; }
 warn () { printf "%b%s%b" "\E[1;31m❯ " "Warning: ${1:-}" "\E[0m\n" >&2; }
 
 service cuttlefish-host-resources start
 
-HOME=/root/cuttlefish /root/cuttlefish/bin/launch_cvd --vm_manager=qemu_cli --start_webrtc=false
+HOME=$CF_HOME $CF_HOME/bin/launch_cvd --vm_manager=qemu_cli --start_webrtc=false
 
